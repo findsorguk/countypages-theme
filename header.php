591f608ee11c8a263fdc5e20f9ed862e81d2e5ef
Mary Chester-Kadwell <mchesterkadwell@gmail.com>
2015-04-10 10:49:25 +0100

    Add breadcrumbs

--- header.php
+++ header.php
@@ -41,7 +41,13 @@
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div><!-- .site-branding -->
 
-
+        <div class="breadcrumbs" xmlns:v="http://rdf.data-vocabulary.org/#">
+            You are here:
+            <?php if(function_exists('bcn_display'))
+            {
+                bcn_display();
+            }?>
+        </div>
 
             <!-- WordPress HTML -->
             <nav id="site-navigation" class="main-navigation" role="navigation">
